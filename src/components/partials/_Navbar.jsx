6a39a3bc5845b05a8a0a3d55c64703403e49bf9d
RosarioManny <rosario.emm47@gmail.com>
2024-11-29 15:56:55 -0500

    TailwindCSS Added

--- src/components/partials/_Navbar.jsx
+++ src/components/partials/_Navbar.jsx
@@ -1,24 +1,25 @@
 // FIRE
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { theme } from "../../styles/style";
 
 const Navbar = () =>  {
   return (
-    <nav>
-    <h3>
-    <Link to="/">
-      <p>E.R.</p>
-    </Link>
-    </h3>
-    <Link to="/projects">
-      <p>Projects</p>
-    </Link>
-    <Link to="/contacts">
-      <p>Contacts</p>
-    </Link>
-    <Link to="/about">
-      <p>About</p>
-    </Link>
+    <nav className={`${theme.nav.wrapper}p-4 mb-6 bg-sunglow-50/50 border-l-4 border-sunglow-500`} >
+      <h3>
+        <Link to="/">
+          <p>E.R.</p>
+        </Link>
+      </h3>
+      <Link to="/projects">
+        <p>Projects</p>
+      </Link>
+      <Link to="/contacts">
+        <p>Contacts</p>
+      </Link>
+      <Link to="/about">
+        <p>About</p>
+      </Link>
     </nav>
   )
 }
---
+++ src/styles/style.js
@@ -0,0 +1,6 @@
+export const theme = {
+  nav: {
+    wrapper:
+    "bg-prussian_blue-500 p-4 shadow-lg fixed top-0 left-0 right-0 z-50 backdrop-blur-sm bg-opacity-95",
+  }
+}
