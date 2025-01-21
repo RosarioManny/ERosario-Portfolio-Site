59b04505c4566cc6bd33230b2fba75d6495fb33a
RosarioManny <rosario.emm47@gmail.com>
2025-01-20 19:24:41 -0500

    Images Fix 5

--- src/components/partials/_Footer.jsx
+++ src/components/partials/_Footer.jsx
@@ -30,13 +30,13 @@ const Footer = () => {
                 <h1 className={`font-semibold text-2xl py-4 ${darkMode ? `${theme.darkMode.highlight}`:`${theme.lightMode.highlight}`}`}> Socials:</h1>
                 <div className={`grid grid-cols-3 gap-7`}>
                   <Link to="https://www.instagram.com/it_sjustman.ny/">
-                    <img className="h-10 hover:animate-pulse" src={`${import.meta.env.BASE_URL}assets/Logos/Instagram_Glyph_White.png`} alt="" />
+                    <img className="h-10 hover:animate-pulse" src={`${import.meta.env.BASE_URL}/assets/Logos/Instagram_Glyph_White.png`} alt="" />
                   </Link>
                   <Link to='https://github.com/RosarioManny'>
                     <img className="h-10 hover:animate-pulse" src="../../assets/Logos/github-mark-white.png" alt="" />
                   </Link>
                   <Link to='https://www.linkedin.com/in/emmanuel-rosario-mannys/'>
-                    <img className="h-10 hover:animate-pulse" src="/Users/emmanuelr/Projects/myWebsite.React/src/assets/Logos/In-White-96.png" alt="" />
+                    <img className="h-10 hover:animate-pulse" src="./assets/Logos/In-White-96.png" alt="" />
                   </Link>
                 </div>
               </div>
